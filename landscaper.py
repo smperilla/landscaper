da0ee9dfe8448de16aa944f64e33695aef0e8b0d
sperilla <smperilla@gmail.com>
2023-10-15 18:41:45 -0400

    landscaer: user can buy push lawnmower

--- landscaper.py
+++ landscaper.py
@@ -15,10 +15,26 @@ while True:
 
     print("Make a choice:")
     print("1. Cut grass with teeth")
+    print("2. Buy scissors")
+
 
     choice = input()
 
     if choice == "1":
         money += 1
         print("You've cut grass with your teeth.")
+    
+    elif choice == '2':
+        if money >= 5:
+            money -= 5
+            tools.append('rusty scissors')
+            print("You've bought rusty scissors.")
+        else:
+            print("You don't have enough money!")
+
+    elif choice == '3':
+        if money >= 25:
+            money -= 25
+            tools.append('old-timey push lawnmower')
+            print("You've bought an old-timey push lawnmower.")
 
